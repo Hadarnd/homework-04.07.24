658a146d0526d799c4a1dc7ea81a9000a0d304e4
Hadar <hadarnd8@gmail.com>
2024-07-13 00:58:18 +0300

    Add files via upload

---
+++ homework2.3.py
@@ -0,0 +1,13 @@
+#start
+
+first_name = str(input("what is your name?"))
+print(first_name)
+
+last_name = input("what is your last name?")
+print(last_name)
+
+print("*",first_name,"*",last_name,"*")
+
+print("-",first_name,"-",last_name,"-")
+
+#end
