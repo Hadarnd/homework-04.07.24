9a0d9b6080919892dd11d3086d7f56f4dbc29769
Hadar <hadarnd8@gmail.com>
2024-07-13 01:26:33 +0300

    Add files via upload

---
+++ homework2.4.py
@@ -0,0 +1,13 @@
+#start
+
+a: int = int(input("how many bottles did you collect?"))
+
+b: int = int(input("how many bottles did your friend collect?"))
+
+if a > b:
+    print("herrey! you won! go take your present :)")
+
+else: print(b,"is more then",a,". Therefore, I'm sorry your friend's won, go tell him 'good job!'")
+
+
+#end
