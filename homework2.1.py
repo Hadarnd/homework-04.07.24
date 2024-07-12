99eed23f1e7bb25db60d49908dac74f9aeb42e87
Hadar <hadarnd8@gmail.com>
2024-07-13 00:21:45 +0300

    Add files via upload

---
+++ homework2.1.py
@@ -0,0 +1,16 @@
+#atart
+
+apple = int(input("How many apples did you buy?"))
+print(apple)
+
+banana = int(input("How many bananas did you buy?"))
+print(banana)
+
+peach = int(input("How many peaches did you buy?"))
+print(peach)
+
+print("Total in my bag:", (apple + banana + peach))
+
+print("multiply my fruits:", (apple * banana * peach))
+
+#end
