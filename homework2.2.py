13641e3c85b2e00af6f14f623084eda73db584b8
Hadar <hadarnd8@gmail.com>
2024-07-13 00:41:46 +0300

    Add files via upload

---
+++ homework2.2.py
@@ -0,0 +1,11 @@
+#start
+
+m_age : float = float(input("How old is your mother?"))
+print(m_age)
+
+age : float = float(input("How old are you?"))
+print(age)
+
+print("How old was your mother when you where born?", int(m_age - age))
+
+#end
