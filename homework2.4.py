b95b48aa5e3a83689c47017ec163799a9c642a63
Hadar <hadarnd8@gmail.com>
2024-07-13 01:49:34 +0300

    homework2.4.py

--- homework2.4.py
+++ homework2.4.py
@@ -5,9 +5,9 @@ a: int = int(input("how many bottles did you collect?"))
 b: int = int(input("how many bottles did your friend collect?"))
 
 if a > b:
-    print("herrey! you won! go take your present :)")
+    print("Herrey! you won! go take your present :)")
 
-else: print(b,"is more then",a,". Therefore, I'm sorry your friend's won, go tell him 'good job!'")
+else: print(b,"is more then",a,". Therefore, I'm sorry your friend won, go tell him 'good job!'")
 
 
 #end
